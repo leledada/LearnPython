753a0f93a280d6c5ecb798f8ee65ef1d7b72c746
Guisen <czhf0002@qq.com>
2018-04-14 23:15:47 +0800

    爬糗百的图

--- qiubai/qiushibaike.py
+++ qiubai/qiushibaike.py
@@ -14,15 +14,10 @@ import threading
 
 
 def mkdir(path):
-    # 去除首位空格
     path = path.strip()
-    # 去除尾部 \ 符号
     path = path.rstrip("\\")
-    # 判断路径是否存在
     is_exists = os.path.exists(path)
-    # 判断结果
     if not is_exists:
-        # 如果不存在则创建目录
         os.makedirs(path)
         print(path, ' 创建成功')
         return True
